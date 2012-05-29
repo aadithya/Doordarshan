b77ac6e367199657a60274bd0dc4986ddca66027
aadithya <aadithya.c@gmail.com>
2012-05-29 16:56:07 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doordarshan::Application.config.secret_token = '62b503a67aa708fda838a42ee9e3dcd0dee3cf29d6a45dd8cdabfa61c0e883d958b641e704a114104a26d6682c3b55b4f47a890197265fd219757a2de9af1098'
