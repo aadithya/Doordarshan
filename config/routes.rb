a0d965f8b085e584266b232e7875835f8d1eecad
aadithya <aadithya.c@gmail.com>
2012-05-29 21:25:40 -0400

    Adding about contact and help

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Doordarshan::Application.routes.draw do
 
-   root :to => 'static_pages#home'
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
+  root :to => 'static_pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- test/functional/static_pages_controller_test.rb
+++ test/functional/static_pages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
+  test "should get about" do
+    get :about
     assert_response :success
   end
 
