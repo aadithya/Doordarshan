f828637b579aea8420e154c0939544c5860436e3
aadithya <aadithya.c@gmail.com>
2012-05-29 20:21:43 -0400

    Routing root file to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Doordarshan::Application.routes.draw do
-  get "static_pages/home"
 
+   root :to => 'static_pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +50,6 @@ Doordarshan::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
