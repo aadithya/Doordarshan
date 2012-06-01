b3c5a5e0d3b6941d81c0f6b098b59904306cdd6d
aadithya <aadithya.c@gmail.com>
2012-06-01 01:11:16 -0700

    Changing About to show channel listing

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,5 +1,9 @@
 class ProgramsController < ApplicationController
 def search_results
+if params[:channel] == "true"
+  @programs = Program.find_all_by_channel(params[:search])
+else
   @programs = Program.search params[:search]
+end
 end
 end
