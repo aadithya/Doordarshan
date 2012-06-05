6dcffe0c0a37e252dfd110b26d72477e968a5418
aadithya <aadithya.c@gmail.com>
2012-06-05 10:49:46 -0700

    User signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Doordarshan::Application.routes.draw do
-	resources :Channels
-	resources :Programs
+  resources :users
 
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/programs/search', to: 'programs#search_results'
+  match '/signup', to: 'users#new'
 
-  root :to => 'static_pages#home'
+  root to: 'static_pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120601211108) do
+ActiveRecord::Schema.define(:version => 20120605000602) do
 
   create_table "channels", :force => true do |t|
     t.string   "name"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(:version => 20120601211108) do
     t.datetime "updated_at", :null => false
     t.string   "start"
     t.string   "stop"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
