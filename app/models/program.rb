34fb35e38e556dfa6f787ea0cff1711f2173f7f6
Vijeth Dinesha <vijeth.d@gmail.com>
2012-06-13 03:16:27 +0530

    Fixing code merge issues Adding back files that were deleted accidentally in previous checkin

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,5 +1,7 @@
 class Program < ActiveRecord::Base
   belongs_to :category
   has_many :schedules
+  has_many :subscribe_lists
+  has_many :users , :through => :subscribe_lists
   attr_accessible :description, :image, :title, :category, :schedules
 end
